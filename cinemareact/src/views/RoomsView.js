2fc19beafaca11281f894065de3cbd25ffbb46a1
Kuba Szostko <kubaszostko@gmail.com>
2020-12-15 11:12:51 +0100

    created basic router

---
+++ cinemareact/src/views/RoomsView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function RoomsView() {
+	return (
+		<div>
+			{/* navigation */}
+			<h1>Sale kinowe</h1>
+			{/* rooms list */}
+		</div>
+	);
+}
+
+export default RoomsView;
