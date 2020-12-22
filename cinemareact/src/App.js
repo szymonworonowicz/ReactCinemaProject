b046c56b52f25fd250df7f9ef73430efe93a139e
Kuba Szostko <kubaszostko@gmail.com>
2020-12-22 17:50:20 +0100

    connected app with redux

--- cinemareact/src/App.js
+++ cinemareact/src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import SeancesView from './views/SeancesView';
 import MoviesView from './views/MoviesView';
 import RoomsView from './views/RoomsView';
@@ -6,12 +8,14 @@ import SeanceDetailsView from './views/SeanceDetailsView';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Route exact path="/" component={SeancesView} />
-      <Route path="/seance/:id" component={SeanceDetailsView} />
-      <Route path="/movies" component={MoviesView} />
-      <Route path="/rooms" component={RoomsView} />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Route exact path="/" component={SeancesView} />
+        <Route path="/seance/:id" component={SeanceDetailsView} />
+        <Route path="/movies" component={MoviesView} />
+        <Route path="/rooms" component={RoomsView} />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
