b14c12bc97cb25d0617b64aeda34c149d2cac871
Kuba Szostko <kubaszostko@gmail.com>
2020-12-23 09:56:17 +0100

    styled Navigation component

--- cinemareact/src/components/Navigation.js
+++ cinemareact/src/components/Navigation.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { AppBar, Toolbar, Button } from '@material-ui/core';
+
+const linkStyle = {
+	color: 'white',
+	textDecoration: 'none',
+};
 
 function Navigation() {
 	return (
-		<nav>
-			<ul>
-				<li>
-					<Link to="/">Seanse</Link>
-				</li>
-
-				<li>
-					<Link to="/movies">Filmy</Link>
-				</li>
-
-				<li>
-					<Link to="/rooms">Sale</Link>
-				</li>
-			</ul>
-		</nav>
+		<AppBar position="static">
+			<Toolbar>
+				<Button color="inherit">
+					<Link to="/" style={linkStyle}>Seanse</Link>
+				</Button>
+				<Button color="inherit">
+					<Link to="/movies" style={linkStyle}>Filmy</Link>
+				</Button>
+				<Button color="inherit">
+					<Link to="/rooms" style={linkStyle}>Sale</Link>
+				</Button>
+			</Toolbar>
+		</AppBar>
 	);
 }
 
