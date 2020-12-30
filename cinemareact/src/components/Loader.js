85c8dccbaa1c90464b1c4a1e3fe70e8486165a9c
Kuba Szostko <kubaszostko@gmail.com>
2020-12-30 20:12:07 +0100

    change class to className in Loader component

--- cinemareact/src/components/Loader.js
+++ cinemareact/src/components/Loader.js
@@ -3,7 +3,7 @@ import React from 'react';
 // src: https://loading.io/css/
 function Loader() {
     return (
-        <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+        <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
     );
 }
 
