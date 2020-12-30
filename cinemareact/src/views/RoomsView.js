53a1f813fb7b657b2bd28017eceea4374a92add0
Kuba Szostko <kubaszostko@gmail.com>
2020-12-30 20:40:06 +0100

    styled headers

--- cinemareact/src/views/RoomsView.js
+++ cinemareact/src/views/RoomsView.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
-import { Container } from '@material-ui/core';
+import { Container, Typography } from '@material-ui/core';
 
 function RoomsView() {
 	return (
 		<>
 			<Navigation />
 			<Container maxWidth="lg">
-				<h1>Sale kinowe</h1>
+				<Typography variant="h4" component="h1" style={{
+					padding: '24px 0',
+					borderBottom: '1px solid #DDD',
+					textTransform: 'uppercase'
+				}}>Sale kinowe</Typography>
 				{/* rooms list */}
 			</Container>
 		</>
--- cinemareact/src/views/SeancesView.js
+++ cinemareact/src/views/SeancesView.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
-import { Container } from '@material-ui/core';
+import { Container, Typography } from '@material-ui/core';
 
 function SeancesView() {
 	return (
 		<>
 			<Navigation />
 			<Container maxWidth="lg">
-				<h1>Najnowsze seanse</h1>
+				<Typography variant="h4" component="h1" style={{
+					padding: '24px 0',
+					borderBottom: '1px solid #DDD',
+					textTransform: 'uppercase'
+				}}>Najnowsze seanse</Typography>
 				{/* seances search bar */}
 				{/* seances list */}
 			</Container>
