a4fc99de2612209cdb45fa408c593aeb300517ac
Kuba Szostko <kubaszostko@gmail.com>
2020-12-30 21:25:12 +0100

    adjusted listitem content and listitem and list styles

--- cinemareact/src/components/MovieListItem.js
+++ cinemareact/src/components/MovieListItem.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ListItem } from '@material-ui/core';
+import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
+
+const CardStyle = {
+    flexBasis: '45%',
+    marginRight: '32px',
+    marginBottom: '64px',
+};
 
 function MovieListItem(props) {
     const { film } = props;
 
     // this needs improving of course
     return (
-        <ListItem>{film.title}</ListItem>
+        <Card style={CardStyle}>
+            <CardHeader 
+                title={film.title} 
+                subheader={film.director}
+                style={{
+                    marginBottom: '0',
+                    paddingBottom: '0'
+                }}
+            />
+            <CardContent style={{ marginTop: '0' }}>
+                <Typography variant="body1" style={{ marginBottom: '16px' }}>Czas trwania: {film.time} minut</Typography>
+                <Typography variant="body1">{film.description}</Typography>
+            </CardContent>
+        </Card>
     );
 }
 
