510632cec27230c6c1d2b050a6b687f4e4877956
szymon <szymonworonowicz25@gmail.com>
2021-01-06 23:38:11 +0100

    Seances Form work correctly

--- cinemareact/src/components/ScreeningForm.js
+++ cinemareact/src/components/ScreeningForm.js
@@ -1,160 +1,167 @@
-import React, {useEffect} from 'react';
-import PropTypes from 'prop-types';
-import {useSelector, useDispatch } from 'react-redux';
-import { Formik, Form, Field } from 'formik';
-import { getFilms } from '../redux/film/filmActions';
-import { getHalls } from '../redux/halls/hallsActions'
-import Loader from './Loader';
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { useSelector, useDispatch } from "react-redux";
+import { Formik, Form, Field } from "formik";
+import { getFilms } from "../redux/film/filmActions";
+import { getHalls } from "../redux/halls/hallsActions";
+import Loader from "./Loader";
 // this is just styled input fields
-import { FormikTextField } from 'formik-material-fields';
-import { Button } from '@material-ui/core'
-import * as Yup from 'yup';
-import { addScreening, updateScreening } from '../redux/screening/screeningActions';
+import { FormikTextField } from "formik-material-fields";
+import { Button } from "@material-ui/core";
+import * as Yup from "yup";
+import {
+  addScreening,
+  updateScreening,
+} from "../redux/screening/screeningActions";
 
-const isObjectEmpty = obj => Object.keys(obj).length === 0;
+const isObjectEmpty = (obj) => Object.keys(obj).length === 0;
 
 const FormStyle = {
-    marginBottom: '16px',
-    background: 'white',
-    display: 'flex',
-    flexDirection: 'column',
+  marginBottom: "16px",
+  background: "white",
+  display: "flex",
+  flexDirection: "column",
 };
 const DropDownStyle = {
-    height:'30px',
-    background: 'white'
-
+  height: "30px",
+  background: "white",
 };
 const DropDownElementStyle = {
-    fontsize:'25px',
-}
-
+  fontsize: "25px",
+};
 
 function ScreeningForm(props) {
-    const { screening, closeModalFn } = props;
-    const { loading, films,error } = useSelector(state => state.filmsState);
-    const { halls } = useSelector(state => state.hallsState);
-    const dispatch = useDispatch();
+  const { screening, closeModalFn } = props;
+  const { loading, films, error } = useSelector((state) => state.filmsState);
+  const { halls } = useSelector((state) => state.hallsState);
+  const dispatch = useDispatch();
 
-    
-    useEffect(() => {
-        dispatch(getFilms());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(getFilms());
+  }, [dispatch]);
 
-    useEffect(() => {
-        dispatch(getHalls());
-    },[dispatch]);
+  useEffect(() => {
+    dispatch(getHalls());
+  }, [dispatch]);
 
-    if(loading) return <Loader />;
+  if (loading) return <Loader />;
 
-    if(error) return <p>Error: {error}</p>
+  if (error) return <p>Error: {error}</p>;
 
-    const initialValues = {
-       time:screening.startTime,
-       filmId:screening.filmId,
-       hallId:screening.hallId
-    };
-    
-    const validationSchema = Yup.object({
-        filmName: Yup.string().required('Required'),
-        time: Yup.date().min(new Date())
-                                    .max(new Date('01-01-2050'))
-                                    .required('Required'),
-        hallId: Yup.string().required('Required'),
-    });
+  const initialValues = {
+    time: screening.startTime,
+    filmId: screening.filmId,
+    hallId: screening.hallId,
+  };
 
-    const handleSubmit = values => {
-        if(isObjectEmpty(screening)) {
-            dispatch(addScreening(values));
-        } else {
-            const updatedScreening = {
-                ...screening,
-                ...values,
-            };
+  const validationSchema = Yup.object({
+    filmId: Yup.string().required("Required"),
+    time: Yup.date()
+      .min(new Date())
+      .max(new Date("01-01-2050"))
+      .required("Required"),
+    hallId: Yup.string().required("Required"),
+  });
 
-            dispatch(updateScreening(updatedScreening));
-        }
+  const handleSubmit = (values) => {
+    if (isObjectEmpty(screening)) {
+      dispatch(addScreening(values));
+    } else {
+      const updatedScreening = {
+        ...screening,
+        ...values,
+      };
 
-        closeModalFn();
+      dispatch(updateScreening(updatedScreening));
     }
 
-    return (
-        <Formik
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            onSubmit={handleSubmit}
-        >
-            {formik => (
-                <Form style={FormStyle}>
-                    <Field
-                        component="select"
-                        id="filmId"
-                        name="filmId"
-                        style={DropDownStyle}>
-                    {
-                        films.map(film => {
-                            return (
-                                <option key={film.id} value={film.id} style={DropDownElementStyle}>
-                                    {film.id} {film.title}
-                                </option>
-                            )
-                        })
-                    }
+    closeModalFn();
+  };
 
-                    </Field>
-                    <FormikTextField
-                        type="date"
-                        id="time"
-                        name="time"
-                        margin="normal"
-                    />
-
-                    <Field
-                    component="select"
-                    id="hallId"
-                    name="hallId"
-                    style={DropDownStyle}>
-                        {
-                            halls.map(hall => {
-                                return (
-                                    <option key={hall.id} value={hall.id} style={DropDownElementStyle}>
-                                        Sala numer {hall.id}
-                                    </option>
-                                )
-                            })
-                        }
-                    </Field>
-                    <Button
-                        style={{ marginTop: '32px' }}
-                        type="submit"
-                        variant="contained"
-                        color="primary"
-                        //disabled={!formik.isValid}
-                    >
-                        Zapisz
-                    </Button>
-                </Form>
-            )}
-        </Formik>
-    );
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      {(formik) => (
+        <Form style={FormStyle}>
+          <Field
+            component="select"
+            id="filmId"
+            name="filmId"
+            style={DropDownStyle}
+            label="Select film"
+          >
+            {films.map((film) => {
+              return (
+                <option
+                  key={film.id}
+                  value={film.id}
+                  style={DropDownElementStyle}
+                >
+                  {film.id} {film.title}
+                </option>
+              );
+            })}
+          </Field>
+          <FormikTextField
+            type="datetime-local"
+            id="time"
+            name="time"
+            margin="normal"
+          />
+          <Field
+            component="select"
+            id="hallId"
+            name="hallId"
+            style={DropDownStyle}
+            label="Select Hall"
+          >
+            {halls.map((hall) => {
+              return (
+                <option
+                  key={hall.id}
+                  value={hall.id}
+                  style={DropDownElementStyle}
+                >
+                  Sala numer {hall.id}
+                </option>
+              );
+            })}
+          </Field>
+          <Button
+            style={{ marginTop: "32px" }}
+            type="submit"
+            variant="contained"
+            color="primary"
+            disabled={!formik.isValid}
+          >
+            Zapisz
+          </Button>
+        </Form>
+      )}
+    </Formik>
+  );
 }
 
 ScreeningForm.propTypes = {
-    closeModalFn: PropTypes.func.isRequired,
-    screening: PropTypes.shape({
-        // maybe here we can add custom props function to check for minimal length or smth like this
-        filmId: PropTypes.string.isRequired,
-        startTime: PropTypes.instanceOf(new Date()),
-        hallId: PropTypes.string
-    }),
+  closeModalFn: PropTypes.func.isRequired,
+  screening: PropTypes.shape({
+    // maybe here we can add custom props function to check for minimal length or smth like this
+    filmId: PropTypes.string.isRequired,
+    startTime: PropTypes.instanceOf(new Date()),
+    hallId: PropTypes.string,
+  }),
 };
 
 // when we are adding a new movie we don't have to pass any props
 ScreeningForm.defaultProps = {
-    screening: {
-        startTime: new Date(),
-        hallId: '1',
-        filmId: '1',
-    },
+  screening: {
+    startTime: new Date(),
+    hallId: "0",
+    filmId: "0",
+  },
 };
 
 export default ScreeningForm;
