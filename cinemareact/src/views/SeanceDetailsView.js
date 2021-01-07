6cec8a98a2dae5543e7f1b4909021302f6eb073d
SzymonWoronowicz <szymonworonowicz25@gmail.com>
2021-01-07 21:16:09 +0100

    ass SeanceDetailsView without buy ticket

--- cinemareact/src/views/SeanceDetailsView.js
+++ cinemareact/src/views/SeanceDetailsView.js
@@ -2,7 +2,9 @@ import React , { useEffect } from 'react';
 import Navigation from '../components/Navigation';
 import { useSelector, useDispatch } from 'react-redux';
 import { getScreenings} from '../redux/screening/screeningActions';
-import { Card, Container, Typography,CardHeader} from '@material-ui/core';
+import { Card, Container, Typography,CardHeader,CardContent} from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 import Loader from '../components/Loader';
 
 const CardStyle = {
@@ -10,8 +12,31 @@ const CardStyle = {
     marginRight: '32px',
     marginBottom: '64px',
 };
+const paperStyle = {
+		padding:'5px',
+		margin:'5px',
+		textAlign: 'center',
+		backgroundcolor:'Red'
+	}
+const rowStyle = {
+	fontSize :'18px',
+	textAlign: 'center',
+	padding:'5px',
+	margin:'5px',
+	borderColor: "#FFF"
+}
+
+
+const SeatingValid = {
+	color:'green'
+}
+const SeatingInValid = {
+	color :'red'
+}
+
 function SeanceDetailsView(props) {
 	const id = props.match.params.id;
+	
 
 	const { loading, screenings, error } = useSelector(state => state.screeningState);
 	const dispatch = useDispatch();
@@ -25,9 +50,56 @@ function SeanceDetailsView(props) {
 
     if(error) return <p>Error: {error}</p>
 
+    const getStringFromDate = (date) => {
+        let dateFromString = new Date(date);
+        return ("00" + (dateFromString.getUTCDate())).slice(-2) + "."+ ("00" + (dateFromString.getMonth()+1)).slice(-2) + "."+dateFromString.getFullYear()+ "  " 
+            + dateFromString.getHours()+":" + dateFromString.getMinutes();
+        
+	}
+	// const useStyles = makeStyles((theme) => ({
+	// 	root: {
+	// 	  flexGrow: 1,
+	// 	},
+	// 	,
+	//   }));
+	
+	// eslint-disable-next-line eqeqeq
 	const screening = screenings.filter(x => x.id == id)[0];
+
+	const isSeatingValid =(id) => {
+		// eslint-disable-next-line eqeqeq
+		var elem = screening.tickets.find(x => x.id == id);
+		// eslint-disable-next-line eqeqeq
+		if(elem == undefined) {
+			return false;
+		} return true;
+	}
+
+	//const classes = useStyles();
+	let seetingslist = [];
+	for(let i = 0; i<screening.hall.capacity/10+1;i++) {
+		let row = [];
+		for(let j=0;j<11;j++) {
+			if(j===0) {
+				row.push(
+					<Grid key={i*10+j} item  xs={0.2}  >
+						<Paper variant='outlined' style={rowStyle}>{String.fromCharCode(65+i)}</Paper>
+					</Grid>
+				)
+			}
+			else {
+				row.push(
+					<Grid key={i*10+j} item  xs={0.2} >
+						<Paper component={'button'}  key={`0${i*10+j}`} style={paperStyle} >{j==10?j:`${j}`} </Paper>
+					</Grid>
+				)
+			}
+
+		}
+		seetingslist.push(row);
+	}
 	return (
-		<>
+		<div style={{position: 'relative'}}>
 			<Navigation />
 			<Container maxWidth="lg">
 				<Typography variant="h4" component="h1" style={{
@@ -45,12 +117,29 @@ function SeanceDetailsView(props) {
                     style={{
                         marginBottom: '0',
                         paddingBottom: '0'
-                    }}
-                />
+                    }}/>
+                	<CardContent style={{ marginTop: '0' }}>
+						<Typography variant="body1" style={{ marginBottom: '16px' }}>Czas rozpoczęcia: {getStringFromDate(screening.startTime)}</Typography>
+						<Typography variant="body1" style={{ marginBottom: '16px' }}>Nr sali: {screening.hallId}</Typography>
+						<Typography variant="body1" style={{ marginBottom: '16px' }}>Ilość miejsc: {screening.hall.capacity}</Typography>
+						<Typography variant="body1" style={{ marginBottom: '16px' }}>Czas trwania: {screening.film.time} h</Typography>
+						<Typography variant="body1" style={{ marginBottom: '16px' }}>Ilość dostępnych biletów: {screening.hall.capacity - screening.tickets.length} </Typography>
+					</CardContent>
 				</Card>
+				<div style={{position: 'absolute', top:'110%', left:'40%', margin:'margin: -25px 0 0 -25px'}} >
+
+					<Grid container spacing={2}>
+						{
+							seetingslist.map(value => (
+								<Grid  container item xs={10}  >
+									{value}
+								</Grid>
+							))}
+					</Grid>
+				</div>
 				{/* buy ticket form	*/}
 			</Container>
-		</>
+		</div>
 	);
 }
 
