0012585c52f6919cfda07072a77a040654a00408
SzymonWoronowicz <szymonworonowicz25@gmail.com>
2021-01-12 15:10:31 +0100

    Add DateFilter for Screening and popularity of films

--- NetCoreAPI/Controllers/ScreeningController.cs
+++ NetCoreAPI/Controllers/ScreeningController.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -16,80 +19,89 @@ namespace NetCoreAPI.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> getScreenings()
+        public async Task<IActionResult> getScreenings([FromQuery] DateTime current, [FromQuery] DateTime today)
         {
             if (await Context.Screenings.AnyAsync())
             {
                 var screenings = await Context.Screenings.Include(x => x.Film)
-                                                    .Include(x => x.Hall)
-                                                    .Include(x => x.Tickets)
-                                                    .ToListAsync();
+                                                                        .Include(x => x.Hall)
+                                                                        .Include(x => x.Tickets)
+                                                                        .ToListAsync();
+                if (current != default(DateTime))
+                {
+                    screenings = screenings.Where(x => x.StartTime <= current && x.StartTime.AddHours(x.Film.Time) >= current).ToList();
+                }
+                else if (today != default(DateTime))
+                {
+                    screenings = screenings.Where(x => x.StartTime.Date == today.Date).ToList();
+                }
+
                 screenings.Sort((x, y) => x.StartTime.CompareTo(y.StartTime));
-            return Ok(new { screenings = screenings });
-        }
+                return Ok(new { screenings = screenings });
+            }
 
             return BadRequest("Nie ma zadnego seansu w bazie");
-    }
+        }
 
-    [HttpGet("{id}")]
-    public async Task<IActionResult> getScreening([FromRoute] int id)
-    {
-        if (await Context.Screenings.AnyAsync())
+        [HttpGet("{id}")]
+        public async Task<IActionResult> getScreening([FromRoute] int id)
         {
-            if (id != 0)
+            if (await Context.Screenings.AnyAsync())
             {
-                var screening = await Context.Screenings.Include(x => x.Film)
-                                                    .Include(x => x.Hall)
-                                                    .Include(x => x.Tickets)
-                                                    .FirstOrDefaultAsync(x => x.Id == id);
+                if (id != 0)
+                {
+                    var screening = await Context.Screenings.Include(x => x.Film)
+                                                        .Include(x => x.Hall)
+                                                        .Include(x => x.Tickets)
+                                                        .FirstOrDefaultAsync(x => x.Id == id);
 
-                
 
-                return Ok(new { screening = screening });
 
+                    return Ok(new { screening = screening });
+
+                }
             }
+
+            return BadRequest("Nie ma seansu o takim id w bazie");
         }
 
-        return BadRequest("Nie ma seansu o takim id w bazie");
-    }
-
-    [HttpPost]
-    public async Task<IActionResult> addScreening([FromBody] Screening screening)
-    {
-        if (await Context.Screenings.AnyAsync(x => x.StartTime == screening.StartTime) == false)
+        [HttpPost]
+        public async Task<IActionResult> addScreening([FromBody] Screening screening)
         {
-            await Context.Screenings.AddAsync(screening);
-
-            await Context.SaveChangesAsync();
-            var addedScreening = await Context.Screenings.Include(x => x.Hall).Include(x => x.Film).FirstOrDefaultAsync(x => x.Id == screening.Id);
-
-            return StatusCode(201, new { screening = addedScreening });
-        }
-
-        return BadRequest("Seans istnieje w bazie");
-    }
-
-    [HttpPut]
-    public async Task<IActionResult> updateScreening([FromBody] Screening screening)
-    {
-        if (await Context.Screenings.AnyAsync(x => x.Id == screening.Id))
-        {
-            try
+            if (await Context.Screenings.AnyAsync(x => x.StartTime == screening.StartTime && x.HallId==screening.HallId ) == false)
             {
-                Context.Screenings.Update(screening);
+                await Context.Screenings.AddAsync(screening);
 
                 await Context.SaveChangesAsync();
+                var addedScreening = await Context.Screenings.Include(x => x.Hall).Include(x => x.Film).FirstOrDefaultAsync(x => x.Id == screening.Id);
 
-                return Ok();
+                return StatusCode(201, new { screening = addedScreening });
             }
-            catch (System.Exception)
-            {
 
-                throw;
-            }
+            return BadRequest("Seans istnieje w bazie");
         }
 
-        return BadRequest("Dany seans nie istnieje w bazie");
+        [HttpPut]
+        public async Task<IActionResult> updateScreening([FromBody] Screening screening)
+        {
+            if (await Context.Screenings.AnyAsync(x => x.Id == screening.Id))
+            {
+                try
+                {
+                    Context.Screenings.Update(screening);
+
+                    await Context.SaveChangesAsync();
+
+                    return Ok();
+                }
+                catch (System.Exception)
+                {
+
+                    throw;
+                }
+            }
+
+            return BadRequest("Dany seans nie istnieje w bazie");
+        }
     }
-}
 }
