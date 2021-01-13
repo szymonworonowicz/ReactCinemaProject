12a23776e46067dd38c1a2cd02cc2f67944e0ca1
Kuba Szostko <kubaszostko@gmail.com>
2021-01-13 11:17:31 +0100

    foxed time dispay in NewSeanceDetails

--- cinemareact/src/views/NewSeanceDetailsView.js
+++ cinemareact/src/views/NewSeanceDetailsView.js
@@ -11,6 +11,9 @@ function NewSeanceDetailsView(props) {
 	const id = props.match.params.id;
 	const screening = props.location.state.screening;
 
+	const rawDate = new Date(screening.startTime);
+	const adjustedDate = `${rawDate.getDate() < 10 ? '0' : ''}${rawDate.getDate()}.${rawDate.getMonth() + 1 < 10 ? '0' : ''}${rawDate.getMonth() + 1}.${rawDate.getFullYear()} ${rawDate.getHours()}:${rawDate.getMinutes() < 10 ? '0' : ''}${rawDate.getMinutes()}`;
+
 	return (
 		<>
 			<Navigation />
@@ -27,7 +30,7 @@ function NewSeanceDetailsView(props) {
 						style={{ marginBottom: '0', paddingBottom: '0' }}
 					/>
 					<CardContent>
-						<Typography variant="body1">Czas rozpoczęcia: {screening.startTime}</Typography>
+						<Typography variant="body1">Czas rozpoczęcia: {adjustedDate}</Typography>
 						<Typography variant="body1">Sala numer: {screening.hallId}</Typography>
 						<Typography variant="body1">Ilość miejsc: {screening.hall.capacity}</Typography>
 						<Typography variant="body1">Dostępne bilety: {screening.hall.capacity - screening.tickets.length}</Typography>
